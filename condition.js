14a617b57c0fa140270ed3dba0ef87cb488f774b
AnweshKadimetla <96062793+AnweshKadimetla@users.noreply.github.com>
2022-02-01 06:50:36 +0530

    Add files via upload

---
+++ condition.js
@@ -0,0 +1,7 @@
+//If check the number is positive or negative
+const number=prompt("Enter a number:")
+
+//Check if number is >0 : positive
+if(number>0){
+    console.log("Number is positive")
+}
---
+++ variables.js
@@ -0,0 +1,46 @@
+var x=10;
+let xx=20;
+var xxx=5,y=10,z=15;
+let xxxx=1,yy=2,zz=3;
+var d=1.000657;
+let pi=3.14;
+console.log(x);
+console.log(y);
+console.log(d);
+console.log(pi);
+let string="Hello World";
+var numbers=123456;
+console.log(numbers);
+console.log(string);
+//comments
+//Boolean, null, numbers
+var string1='sachin'//string
+var str="Tendulkar"//string
+var BigInt=26416984365846//BigInt
+var Boolean=true//Boolean
+var Bool=false//Boolean
+var a;//Undefined
+let value=Symbol('hello')//symbol
+var val=Symbol("World")//symbol
+console.log(str);
+let student={
+    id:1,
+    fname:'sachin',
+    lname:'null',
+    class:10
+};//Object
+//Note: Except object all the datatypes are primitive.
+console.log(student)
+console.log(typeof(student))
+
+//Javascript Operators
+let x1=2;
+let y1=2;
+console.log(x1==y1)//True
+console.log(x1===y1)//true'==='Returns true if only if datatypes are must from the same data
+//If the operands are the equals of the same data
+
+//Data types are same but values are defferent
+let a1=10;
+let b1='abc';
+console.log(a1===b1);//false
